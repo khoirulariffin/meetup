fb57a813cafdaed73f6d273b685a319348cb4378
khoirulariffin <khoirulariffin@gmail.com>
2023-07-24 16:53:17 +0700

    skeleton

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/4/45/Museum_Nasional_Indonesia.jpg",
+    address: "Jakarta",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e3/Istana-Merdeka.jpg",
+    address: "Jakarta",
+    description: "This is a second meetup!",
+  },
+];
+
+export default function Home() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
